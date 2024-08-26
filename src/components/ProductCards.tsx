e2c358924359137be258dde6e6165207fa8b788f
Abdelrahman Sameh <abdosamehdev@gmail.com>
2024-08-26 15:47:33 +0300

    code enhancements

--- src/components/ProductCards.tsx
+++ src/components/ProductCards.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { Card } from "flowbite-react";
 import { ProductData } from "../types";
 import { MainButton } from "./Buttons";
+import { useNavigate } from "react-router-dom";
 import StarRating from "./StarRating";
 import { useCart } from "../hooks/useCart";
 
@@ -9,6 +12,7 @@ type Props = { productData?: ProductData };
 export const MainProductCard: React.FC<Props> = ({
   productData,
 }: Props): JSX.Element => {
+  const navigate = useNavigate();
   const rating = productData ? Math.ceil(productData.rating) : 3;
   const { addToCart } = useCart();
   return (
@@ -48,8 +52,16 @@ export const MainProductCard: React.FC<Props> = ({
                 thumbnail: productData.thumbnail,
                 total: productData.price,
               });
+
+            // navigate("/cart");
           }}
         />
+        {/* <a
+          href="#"
+          className="rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+        >
+          Add to cart
+        </a> */}
       </div>
     </Card>
   );
